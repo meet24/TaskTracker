f78a259b4c1debec832dbc59f9ad815b6624a681
Meet Patel <meet241patel@gmail.com>
2025-04-20 20:21:11 -0400

    Basic Components Setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css'
+import TaskForm from './components/TaskForm'
+import TaskColumn from './components/TaskColumn'
+import todoIcon from './assets/direct-hit.png'
+import doingIcon from './assets/glowing-star.png'
+import doneIcon from './assets/check-mark-button.png'
+
+
+function App() {
+  return (
+    <>
+    <div className='app'>
+      <TaskForm/>
+      <main className='app_main'>
+        <TaskColumn title='To Do' icon={todoIcon} />
+        <TaskColumn title='In Progress' icon={doingIcon} />
+        <TaskColumn title='Completed' icon={doneIcon} />
+      </main>
+    </div>
+    </>
+  )
+}
+export default App
---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./TaskForm.css"
+import Tag from './Tag'
+
+const TaskForm = () => {
+  return (
+    <>
+        <header className='app_header'>
+        <form>
+
+            <input type='text' className='task_input' placeholder='Enter your task' />
+            
+            <div className='task_form_bottom_line'>
+                <div>
+                    <Tag tagName='HTML' />
+                    <Tag tagName="CSS"/>
+                    <Tag tagName="JavaScript"/>
+                    <Tag tagName="React"/>
+                </div>
+                <div>
+                    <select className='task_status'>
+                        <option value='todo'>To Do</option>
+                        <option value='inprogress'>In Progress</option>
+                        <option value='completed'>Completed</option>
+                    </select>
+                    <button type='submit' className='task_submit'>+ Add Task</button>
+                </div>
+            </div>
+        </form>
+        </header>
+    </>
+  )
+}
+
+export default TaskForm
