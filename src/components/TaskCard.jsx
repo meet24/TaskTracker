ad59b0c5a64d39d72a1926d1624982ff60e12639
Meet Patel <meet241patel@gmail.com>
2025-04-20 23:51:37 -0400

    FInal Commit

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import './TaskCard.css'
-import Tag from "./Tag"
-import deleteIcon from '../assets/delete.png'
+import React from "react";
 
-const TaskCard = () => {
-  return (
-    <article className='task_card'>
-      <p className='task_text'>Sample</p>
-      <div className='task_card_bottom_line'>
-        <div className='task_card_tags'>
-          <Tag tagName="HTML" />
-          <Tag tagName="CSS" />
-        </div>
-        <div className='task_delete'>
-          <img src={deleteIcon} className='delete_icon' alt=''></img>
-        </div>
-      </div>
-    </article>
-  )
-}
+import "./TaskCard.css";
+import Tag from "./Tag";
+import deleteIcon from "../assets/delete.png";
 
-export default TaskCard
+const TaskCard = ({ title, tags, handleDelete, index }) => {
+    return (
+        <article className='task_card'>
+            <p className='task_text'>{title}</p>
+
+            <div className='task_card_bottom_line'>
+                <div className='task_card_tags'>
+                    {tags.map((tag, index) => (
+                        <Tag key={index} tagName={tag} selected />
+                    ))}
+                </div>
+                <div
+                    className='task_delete'
+                    onClick={() => handleDelete(index)}>
+                    <img src={deleteIcon} className='delete_icon' alt='' />
+                </div>
+            </div>
+        </article>
+    );
+};
+
+export default TaskCard;
