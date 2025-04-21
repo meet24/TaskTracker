f0fa6c0e6da223a47f1839c5ab70846333e1b2e4
Meet Patel <meet241patel@gmail.com>
2025-04-20 20:30:02 -0400

    TaskForm updated

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,35 +1,60 @@
-import React from 'react'
-import "./TaskForm.css"
-import Tag from './Tag'
+import React, {useState} from "react";
+import "./TaskForm.css";
+import Tag from "./Tag";
 
 const TaskForm = () => {
+
+  const [taskData, setTaskData] = useState({
+    task: "",
+    status: "todo"
+  })
+  
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    
+    setTaskData(prev => {
+      return { ...prev, [name]: value };
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(taskData);
+  }
+  
   return (
-    <>
-        <header className='app_header'>
-        <form>
+    <header className="app_header">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="task"
+          className="task_input"
+          placeholder="Enter your task"
+          onChange={handleChange}
+        />
 
-            <input type='text' className='task_input' placeholder='Enter your task' />
-            
-            <div className='task_form_bottom_line'>
-                <div>
-                    <Tag tagName='HTML' />
-                    <Tag tagName="CSS"/>
-                    <Tag tagName="JavaScript"/>
-                    <Tag tagName="React"/>
-                </div>
-                <div>
-                    <select className='task_status'>
-                        <option value='todo'>To Do</option>
-                        <option value='inprogress'>In Progress</option>
-                        <option value='completed'>Completed</option>
-                    </select>
-                    <button type='submit' className='task_submit'>+ Add Task</button>
-                </div>
-            </div>
-        </form>
-        </header>
-    </>
-  )
-}
+        <div className="task_form_bottom_line">
+          <div>
+            <Tag tagName="HTML" />
+            <Tag tagName="CSS" />
+            <Tag tagName="JavaScript" />
+            <Tag tagName="React" />
+          </div>
 
-export default TaskForm
+          <div>
+            <select name="status" className="task_status" onChange={handleChange}>
+              <option value="todo">To do</option>
+              <option value="inprogress">In Progress</option>
+              <option value="completed">Completed</option>
+            </select>
+            <button type="submit" className="task_submit">
+              + Add Task
+            </button>
+          </div>
+        </div>
+      </form>
+    </header>
+  );
+};
+
+export default TaskForm;
